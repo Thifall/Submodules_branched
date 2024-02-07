70135083e93f23d6d131aa387a455c51aa55a84e
Jan Musioł <migosh@o2.pl>
2024-02-07 12:44:23 +0100

    Created Project

---
+++ Submodules_branched/Submodules_branched/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Submodules_branched
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
